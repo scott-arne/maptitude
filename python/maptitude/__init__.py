01f16b030e0ffce21f49b4bf7136543474db9caf
Scott Arne Johnson <scott.arne.johnson@gmail.com>
2026-04-13 15:35:53 -0700

    bump: version 0.1.3 → 0.1.4

--- python/maptitude/__init__.py
+++ python/maptitude/__init__.py
@@ -32,8 +32,8 @@ import re
 import warnings
 
 # Version info
-__version__ = "0.1.3"
-__version_info__ = (0, 1, 3)
+__version__ = "0.1.4"
+__version_info__ = (0, 1, 4)
 
 
 def _check_openeye_version():
